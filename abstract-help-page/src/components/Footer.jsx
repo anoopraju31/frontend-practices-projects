c6d75bfc269760531059cb849963fbbdc5b19b17
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 21:49:51 +0530

    Abstract Help Page: Footer Section Component - Styles added

--- abstract-help-page/src/components/Footer.jsx
+++ abstract-help-page/src/components/Footer.jsx
@@ -4,7 +4,10 @@ import { FooterSection } from '.'
 const Footer = () => {
   return (
     <div className='w-full bg-black'>
-        <footer className='max-w-[1400px] mx-auto px-6'>
+        <footer className='max-w-[1400px] mx-auto px-6 py-16 grid grid-cols-2  md:grid-cols-4'>
+            <FooterSection />
+            <FooterSection />
+            <FooterSection />
             <FooterSection />
         </footer>
     </div>
--- abstract-help-page/src/components/FooterSection.jsx
+++ abstract-help-page/src/components/FooterSection.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const FooterSection = () => {
+const FooterSection = ({title, items, contact}) => {
   return (
-    <section>FooterSection</section>
+    <section className='text-white'>
+        <h2 className='py-2 text-xl font-bold'> {title} </h2>
+
+        <div>
+            {
+                items.map(item => <p key={item}> item </p>)
+            }
+        </div>
+
+        {
+            contact &&
+            <div className='mt-8'>
+                <h4 className='font-bold'> Contract Us </h4>
+                <p> info@abstract.com </p>
+            </div>
+        }
+    </section>
   )
 }
 
