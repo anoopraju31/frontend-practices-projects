824f3ee287c01952f2ecb009e31a21e3d4e75c40
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 20:55:56 +0530

    Abstract Help Page: Button Component - animation added

--- abstract-help-page/src/components/Button.jsx
+++ abstract-help-page/src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { reveal } from '../utills/animations'
 
 const Button = ({outlined, text}) => {
   return (
-    <div className={`px-6 py-2 text-white font-medium rounded-lg ${outlined? 'border-2 border-white' : 'bg-[#4C5FD5] hover:bg-white hover:text-black'}`}> {text} </div>
+    <motion.div 
+      variants={reveal} 
+      initial="hiddenRightVariant"
+      animate="revealedVariant"
+      className={`px-6 py-2 text-white font-medium rounded-lg ${outlined? 'border-2 border-white' : 'bg-[#4C5FD5] hover:bg-white hover:text-black'}`}> {text} </motion.div>
   )
 }
 
