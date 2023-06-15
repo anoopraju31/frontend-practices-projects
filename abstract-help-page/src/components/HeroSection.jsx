d6ab134ce01db6556358410c94c13b81451bcb70
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 20:47:01 +0530

    Abstract Help Page: Hero Seaction & Search Box Component - animation added

--- abstract-help-page/src/components/HeroSection.jsx
+++ abstract-help-page/src/components/HeroSection.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+
 import { SearchBox } from './'
 
 const HeroSection = () => {
     return (
-        <section className='px-6 py-28 bg-[#dadbf1] flex flex-col justify-center items-center'>
-            <h1 className='text-center text-3xl sm:text-5xl md:text-7xl mb-10'> How can we help? </h1>
+        <motion.section className='px-6 py-28 bg-[#dadbf1] flex flex-col justify-center items-center'>
+            <motion.h1 
+                initial={{scale: 0.3, opacity: 0}}
+                animate={{scale: 1, opacity: 1}}
+                transition={{duration: 1}}
+                className='text-center text-3xl sm:text-5xl md:text-7xl mb-10'> How can we help? </motion.h1>
             <SearchBox />
-        </section>
+        </motion.section>
     )
 }
 
--- abstract-help-page/src/components/SearchBox.jsx
+++ abstract-help-page/src/components/SearchBox.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { AiOutlineArrowRight } from 'react-icons/ai'
+import { motion } from 'framer-motion'
 
 const SearchBox = () => {
     const [text, setText] = useState('')
@@ -8,7 +9,11 @@ const SearchBox = () => {
     }
 
     return (
-        <div className='w-full md:max-w-[720px] p-3 bg-white rounded-md border-[1px] border-black flex justify-between items-center gap-4 focus:bg-[#4C5FD5]'>
+        <motion.div 
+            initial={{scale: 0.3, opacity: 0}}
+            animate={{scale: 1, opacity: 1}}
+            transition={{duration: 1}}
+            className='w-full md:max-w-[720px] p-3 bg-white rounded-md border-[1px] border-black flex justify-between items-center gap-4 focus:bg-[#4C5FD5]'>
             <input  
                 type="text" 
                 className='flex-1 h-8 outline-none text-lg' 
@@ -22,7 +27,7 @@ const SearchBox = () => {
                 onClick={handleClick} 
                 className='hover:text-[#4C5FD5]'
             />
-        </div>
+        </motion.div>
     )
 }
 
