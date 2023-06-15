4bd3d2542929839be9900e9c2f5bcf6fbcfaec83
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 16:26:29 +0530

    Abstract Help Page: Navbar Component - functional toggle Menu with animation

--- abstract-help-page/src/components/Navbar.jsx
+++ abstract-help-page/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { AiOutlineClose } from 'react-icons/ai'
+import { AnimatePresence, motion } from 'framer-motion'
 import { Button, Logo } from './'
 
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false)
     const handletoggle = () => setToggleMenu(prev => !prev)
   return (
-    <div className="bg-black">
-        <nav className='max-w-[1400px] h-[88px] mx-auto px-6 flex items-center justify-between'>
+    <div className="bg-black relative z-50">
+        <nav className='max-w-[1400px] h-[88px] z-50 mx-auto px-6 flex items-center justify-between'>
             {/* logo */}
             <div className='flex gap-3 text-white text-xl font-medium'>
                 <div className='group w-[110px] flex hover:text-gray-400'>
@@ -33,16 +34,23 @@ const Navbar = () => {
                     <GiHamburgerMenu size={22} />
                 }
             </div>
-        
+        </nav>
 
-            {
-                toggleMenu && 
-                <div className="absolute top-[88px] left-0  w-full py-7 bg-black border-t-[1px] border-[#dadbf1] text-white flex flex-col justify-center items-center">
-                    <p className='px-10 py-3 text-lg border-b-[1px] border-[#dadbf1]'> Submit a request </p>
-                    <p className='py-3 text-lg'> Sign In </p>
-                </div>
-            }
-    </nav>
+        <AnimatePresence>
+        {
+            toggleMenu && 
+            <motion.div 
+                initial={{y: -50, opacity: 0}}
+                animate={{y: 0, opacity: 1}}
+                exit={{y: -50, opacity: 0}}
+                transition={{duration: 0.5, type: 'spring', stiffness: 100}}
+                className="absolute top-[88px] left-0 -z-10 w-full py-7 bg-black border-t-[1px] border-[#dadbf1] text-white flex flex-col justify-center items-center"
+            >
+                <p className='px-10 py-3 text-lg border-b-[1px] border-[#dadbf1]'> Submit a request </p>
+                <p className='py-3 text-lg'> Sign In </p>
+            </motion.div>
+        }
+        </AnimatePresence>
     </div>
   )
 }
