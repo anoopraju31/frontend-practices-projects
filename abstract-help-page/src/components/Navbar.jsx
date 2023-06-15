fcd79913e52dc8b96a5fdd4ba7cf246949d06489
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 20:53:20 +0530

    Abstract Help Page: Navbar Component - animation added to logo part

--- abstract-help-page/src/components/Navbar.jsx
+++ abstract-help-page/src/components/Navbar.jsx
@@ -3,24 +3,40 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 import { AiOutlineClose } from 'react-icons/ai'
 import { AnimatePresence, motion } from 'framer-motion'
 import { Button, Logo } from './'
+import { reveal } from '../utills/animations'
 
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false)
     const handletoggle = () => setToggleMenu(prev => !prev)
   return (
     <div className="bg-black relative z-50">
-        <nav className='max-w-[1400px] h-[88px] z-50 mx-auto px-6 flex items-center justify-between'>
+        <motion.nav 
+            initial={{opacity: 0 }} 
+            animate={{opacity: 1 }} 
+            transition={{ duration: 1}} 
+            className='max-w-[1400px] h-[88px] z-50 mx-auto px-6 flex items-center justify-between'>
+            
             {/* logo */}
-            <div className='flex gap-3 text-white text-xl font-medium'>
-                <div className='group w-[110px] flex hover:text-gray-400'>
+            <motion.div 
+                variants={reveal}
+                initial="hiddenLeftVariant"
+                animate="revealedVariant"
+                transition={{
+                    ease: "easeIn",
+                    type: "tween",
+                    staggerChildren: 0.1,
+                    duration: 0.5
+                }}
+                className='flex gap-3 text-white text-xl font-medium'>
+                <motion.div variants={reveal} className='group w-[110px] flex hover:text-gray-400'>
                     <Logo />
                     <span> Abstract </span>
-                </div>
+                </motion.div>
 
-                <div className='px-1 border-l-2 border-l-white'>
+                <motion.div variants={reveal} className='px-1 border-l-2 border-l-white'>
                     Help Center
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
 
             <div className='hidden md:flex gap-4'>
                 <Button outlined text='Submit a request' />
@@ -34,7 +50,7 @@ const Navbar = () => {
                     <GiHamburgerMenu size={22} />
                 }
             </div>
-        </nav>
+        </motion.nav>
 
         <AnimatePresence>
         {
