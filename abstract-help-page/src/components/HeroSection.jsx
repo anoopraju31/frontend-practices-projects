870cb00cb9e0b92b1147f727a85ade84a4ba9d4a
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 16:43:18 +0530

    Abstract Help Page: Search Box Component - Initial

--- abstract-help-page/src/components/HeroSection.jsx
+++ abstract-help-page/src/components/HeroSection.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { SearchBox } from './'
 
 const HeroSection = () => {
-  return (
-    <section className='py-20 bg-[#dadbf1]'>
-        <h1 className='text-center text-3xl sm:text-5xl md:text-7xl'> How can we help? </h1>
-    </section>
-  )
+    return (
+        <section className='py-20 bg-[#dadbf1] flex flex-col justify-center items-center'>
+            <h1 className='text-center text-3xl sm:text-5xl md:text-7xl mb-8'> How can we help? </h1>
+            <SearchBox />
+        </section>
+    )
 }
 
 export default HeroSection
