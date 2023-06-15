dd5588aaf96ce8e2ee4e441ee0756144ff6e8788
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 16:29:00 +0530

    Abstract Help Page: Hero Section Component - initial

--- abstract-help-page/src/components/index.js
+++ abstract-help-page/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Logo } from './Logo'
 export { default as Navbar } from './Navbar'
 export { default as Button } from './Button'
+export { default as HeroSection } from './HeroSection'
