5e00d1af5149fc46ec0ddb5eb85fcb474befbcad
anoopraju31 <anoopraju31@gmail.com>
2023-06-15 15:32:47 +0530

    Abstract Help Page: Navbar Component - Logo Added to Navbar

--- abstract-help-page/src/components/Navbar.jsx
+++ abstract-help-page/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Logo } from './'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="bg-black">
+        <nav className='max-w-[1400px] h-[88px] mx-auto flex items-center'>
+        {/* logo */}
+        <div className='flex gap-3 text-white text-xl font-medium'>
+            <div className='group w-[110px] flex hover:text-gray-400'>
+                <Logo />
+                <span> Abstract </span>
+            </div>
+
+            <div className='px-1 border-l-2 border-l-white'>
+                Help Center
+            </div>
+        </div>
+    </nav>
+    </div>
   )
 }
 
