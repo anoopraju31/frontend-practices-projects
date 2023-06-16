459edde21f77bb1d67a9641dc214062d0db72da6
anoopraju31 <anoopraju31@gmail.com>
2023-06-16 11:22:59 +0530

    Abstract Help Page: Footer  Component - Animation Added

--- abstract-help-page/src/components/Footer.jsx
+++ abstract-help-page/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import { FooterSection, Logo } from '.'
 import { sections } from '../utills/footerSections'
+import { reveal } from '../utills/animations'
 
 const Footer = () => {
   return (
@@ -12,17 +14,28 @@ const Footer = () => {
               }
             </div>
 
-            <div className='mt-10 ml-4 md:mt-40 flex flex-col gap-6 text-white'>
+            <motion.div 
+              className='mt-10 ml-4 md:mt-40 flex flex-col gap-6 text-white'
+              variants={reveal}
+              initial='hiddenLeftVariant'
+              whileInView='revealedVariant'
+              transition={{duration: 1}}
+            >
               <div className='w-[138px]'>
                 <Logo />
               </div>
 
-              <div>
+              <motion.div
+                variants={reveal}
+                initial='hiddenLeftVariant'
+                whileInView='revealedVariant'
+                transition={{duration: 1}} 
+              >
                 <p> © Copyright 2023 </p>
                 <p> Abstract Studio Design, Inc. </p>
                 <p> All rights reserved </p>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
         </footer>
     </div>
   )
