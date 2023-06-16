a2400bf4bfa060554270c5c005c954f60140bf4b
anoopraju31 <anoopraju31@gmail.com>
2023-06-16 18:02:15 +0530

    Ableton: Logo Components - initial

--- ableton/src/components/Navbar.jsx
+++ ableton/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Logo } from './'
 
 const Navbar = () => {
   return (
     <nav className='px-5 flex justify-between items-center'>
       <div className='flex gap-6'>
         <div>
-          
+          <Logo />
         </div>
 
         <div>
