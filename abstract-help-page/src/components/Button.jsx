827de539d14726ed4187f06b632dc7ad3984f2d0
anoopraju31 <anoopraju31@gmail.com>
2023-06-16 11:25:08 +0530

    Abstract Help Page: Button Component - Animation duration Added

--- abstract-help-page/src/components/Button.jsx
+++ abstract-help-page/src/components/Button.jsx
@@ -8,6 +8,7 @@ const Button = ({outlined, text}) => {
       variants={reveal} 
       initial="hiddenRightVariant"
       animate="revealedVariant"
+      transition={{ duration: 1}} 
       className={`px-6 py-2 text-white font-medium rounded-lg ${outlined? 'border-2 border-white' : 'bg-[#4C5FD5] hover:bg-white hover:text-black'}`}> {text} </motion.div>
   )
 }
