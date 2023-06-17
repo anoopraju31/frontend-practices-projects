36744d739ef85cd917be70d5f3560c62c20bf65f
anoopraju31 <anoopraju31@gmail.com>
2023-06-17 15:25:37 +0530

    Ableton: Hero Section Component - hero text added

--- ableton/src/components/HeroSection.jsx
+++ ableton/src/components/HeroSection.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className='mx-[8.33vw]  min-h-[var(--hero-section-height)] bg-no-repeat bg-cover bg-pos bg-hero-image'>
-        
+    <div className='mx-[8.33vw] min-h-[var(--hero-section-height)] bg-no-repeat bg-cover bg-pos bg-hero-image flex justify-center items-center'>
+        <h1 className='text-red-500 text-5xl sm:text-6xl md:text-7xl lg:text-9xl font-medium'> Ableton </h1>
     </div>
   )
 }
