edd9661b89a0460e38aa4690ddb00eafc8d6250b
anoopraju31 <anoopraju31@gmail.com>
2023-06-19 14:35:21 +0530

    Ableton: footer items data added in utills

---
+++ ableton/src/utills/footerItems.js
@@ -0,0 +1,65 @@
+export const footerItems = [
+	{
+		id: 1,
+		title: '',
+		links: [
+			{
+				id: 1,
+				title: 'Register Live or Push',
+				link: '/register',
+			},
+			{
+				id: 2,
+				title: 'About Ableton',
+				link: '/about',
+			},
+			{
+				id: 3,
+				title: 'Jobs',
+				link: '/jobs',
+			},
+		],
+	},
+	{
+		id: 2,
+		title: 'Education',
+		links: [
+			{
+				id: 1,
+				title: 'Offers for students and teachers',
+				link: '/offers',
+			},
+			{
+				id: 2,
+				title: 'Ableton for the Classroom',
+				link: '/classroom',
+			},
+			{
+				id: 3,
+				title: 'Ableton for Colleges and Universities',
+				link: '/colleges',
+			},
+		],
+	},
+	{
+		id: 3,
+		title: 'Community',
+		links: [
+			{
+				id: 1,
+				title: 'Find Ableton User Groups',
+				link: '/find',
+			},
+			{
+				id: 2,
+				title: 'Find Certified Training',
+				link: '/training',
+			},
+			{
+				id: 3,
+				title: 'Become a Certified Trainer',
+				link: '/trainer',
+			},
+		],
+	},
+]
