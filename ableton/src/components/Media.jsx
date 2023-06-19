f1350275c3f951d6b237aa370998b0149dfafb4c
anoopraju31 <anoopraju31@gmail.com>
2023-06-19 18:03:02 +0530

    Ableton: Media Collage Component - animation added

--- ableton/src/components/Media.jsx
+++ ableton/src/components/Media.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { motion} from 'framer-motion'
 import mediaImg from '../assets/img/poster-meet-the-makers.avif'
+import { textPop } from '../utills/animations'
 
 const Media = () => {
   return (
-    <div className='w-[83.34vw] xl:w-[1333.44px] mx-auto'>
+    <motion.div 
+      variants={textPop} 
+      initial='start' 
+      whileInView='stop'  
+      transition={{duration: 1.5}}
+      className='w-[83.34vw] xl:w-[1333.44px] mx-auto'
+    >
         <img className='w-full h-auto' src={mediaImg} alt="" />
-    </div>
+    </motion.div>
   )
 }
 
