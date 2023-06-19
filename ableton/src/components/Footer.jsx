2dbf76a9fc4783c55cc864478a8dc8c8eefa2524
anoopraju31 <anoopraju31@gmail.com>
2023-06-19 14:27:58 +0530

    Ableton: Footer Component - logo added

--- ableton/src/components/Footer.jsx
+++ ableton/src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='p-[8.33vw] 2xl:p-[133.28px] bg-white border-t-2 border-gray-200'>
+        <div>
+            <h1 className='text-xl md:text-3xl font-bold mb-[4.166vw] 2xl:mb-66.656px]'> Ableton </h1>
+        </div>
+    </div>
   )
 }
 
