314f9c9cee703f6346a6d91558abbbd3967f178a
anoopraju31 <anoopraju31@gmail.com>
2023-06-19 15:16:15 +0530

    Ableton: Footer Component - footer links added

--- ableton/src/components/Footer.jsx
+++ ableton/src/components/Footer.jsx
@@ -26,19 +26,37 @@ const FooterLinks = ({title, links}) => (
 const Footer = () => {
   return (
     <div className='p-[8.33vw] 2xl:p-[133.28px] bg-white border-t-2 border-gray-200'>
-        <div>
-            <h1 className='text-xl md:text-3xl font-bold mb-[4.166vw] 2xl:mb-66.656px]'> Ableton </h1>
+        <div className='mb-[4.166vw] 2xl:mb-[66.656px]'>
+            <h1 className='text-xl md:text-3xl font-bold]'> Ableton </h1>
         </div>
 
-        <div className=''>
-            <div>
+        <div className="flex flex-wrap">
+            <div className='w-[30%] mb-[4.166vw] 2xl:mb-[66.656px] rp-2'>
+                <div>
+                    <FooterLinks title={footerItems[0].title} links={footerItems[0].links} />
+                </div>
+                <div className='mt-4 flex gap-3'>
+                    <Icon background='bg-[#3477F2]' link='https://www.facebook.com' MediaIcon={<FaFacebook />} />
+                    <Icon background='bg-[#1DA1F2]' link='https://www.facebook.com' MediaIcon={<FaTwitter />} />
+                    <Icon background='bg-[#FF001D]' link='https://www.facebook.com' MediaIcon={<FaYoutube />} />
+                    <Icon background='bg-[#F77737]' link='https://www.facebook.com' MediaIcon={<FaInstagram />} />
+                </div>
+            </div>
+
+            <div className="w-[30%] mb-[4.166vw] 2xl:mb-[66.656px] pr-2">
                 <FooterLinks title={footerItems[1].title} links={footerItems[1].links} />
             </div>
-            <div className='flex gap-4'>
-                <Icon background='bg-[#3477F2]' link='https://www.facebook.com' MediaIcon={<FaFacebook />} />
-                <Icon background='bg-[#1DA1F2]' link='https://www.facebook.com' MediaIcon={<FaTwitter />} />
-                <Icon background='bg-[#FF001D]' link='https://www.facebook.com' MediaIcon={<FaYoutube />} />
-                <Icon background='bg-[#F77737]' link='https://www.facebook.com' MediaIcon={<FaInstagram />} />
+
+            
+            <div className="w-[40%] mb-[4.166vw] 2xl:mb-[66.656px] pr-2">
+                <div className='text-lg'>
+                    <h1 className='mb-1 font-bold'> Sign up to newsletter </h1>
+                    <p> Enter your email address to stay up to date with the latest offers, tutorials, downloads, surveys and more. </p>
+                </div>
+            </div>
+            
+            <div className="w-[30%] mb-[4.166vw] 2xl:mb-[66.656px] pr-2">
+                <FooterLinks title={footerItems[2].title} links={footerItems[2].links} />
             </div>
         </div>
     </div>
