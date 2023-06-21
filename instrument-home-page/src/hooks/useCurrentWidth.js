e6fd3398295ede140e8f35650619634bd823221d
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 16:20:38 +0530

    Instrument Home Page: Custom Hook - useCurrentWidth hook created

---
+++ instrument-home-page/src/hooks/useCurrentWidth.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+const getWidth = () =>
+	window.innerWidth ||
+	document.documentElement.clientWidth ||
+	document.body.clientWidth
+
+function useCurrentWidth() {
+	let [width, setWidth] = useState(getWidth())
+
+	useEffect(() => {
+		const resizeListener = () => {
+			setWidth(getWidth())
+		}
+
+		window.addEventListener('resize', resizeListener)
+
+		return () => {
+			window.removeEventListener('resize', resizeListener)
+		}
+	}, [])
+
+	return width
+}
+
+export default useCurrentWidth
