09c45a568bd94fed815e0cb1f6aebfce5fdf71c4
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 18:50:29 +0530

    Instrument Home Page: Navbar Component - open menu turns false if width is large & adjustable background opacity

--- instrument-home-page/src/Pages/Home.jsx
+++ instrument-home-page/src/Pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import img from '../assets/design/C2-instrument.webp'
 
 const Home = () => {
   return (
-    <div className='h-[200vw] pt-nav-height-sm sm:pt-nav-height-md lg:pt-0'>Home</div>
+    <div className='h-[200vw] pt-nav-height-sm sm:pt-nav-height-md lg:pt-0'>
+      <img src={img} className='w-full h-auto' alt="" />
+    </div>
   )
 }
 
