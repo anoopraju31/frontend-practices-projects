53d19b2a8ce7ab7cf183adb7270f44615702a92b
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 17:25:53 +0530

    Instrument Home Page: NavMarquee Component - marquee effect added

--- instrument-home-page/src/components/NavMarquee.jsx
+++ instrument-home-page/src/components/NavMarquee.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiOutlineArrowSmRight } from 'react-icons/hi'
+import { marqueeText } from '../utills/navMarqueeText'
 
-const MarqueeLink = ({title, link}) => (
-    <a href={link} className='flex items-center text-gray-400'>
-        <span className='underline'> {title} </span>
+const MarqueeLink = ({textId}) => (
+    <a href={marqueeText[textId].link} className='flex items-center text-gray-400'>
+        <span className='underline'> {marqueeText[textId].linkText} </span>
         <HiOutlineArrowSmRight size={20} />
     </a>
 )
 
 const NavMarquee = () => {
+	const [id, setId] = useState(0)
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setId((prev) => (prev + 1) % 3)
+		}, 10000)
+
+		return () => clearInterval(interval)
+	}, [])
   return (
     <div className='py-[3px] flex justify-center text-sm'>
-        <p className='mr-2'> 392 MAKERS CREATING THINGS PEOPLE LOVE TO USE. </p>
-        <MarqueeLink title='Learn more' link='/more' />
+        <p className='mr-2'> {marqueeText[id].text} </p>
+        <MarqueeLink textId={id} />
     </div>
   )
 }
---
+++ instrument-home-page/src/utills/navMarqueeText.js
@@ -0,0 +1,20 @@
+export const marqueeText = [
+	{
+		id: 1,
+		text: '392 MAKERS CREATING THINGS PEOPLE LOVE TO USE.',
+		linkText: 'learn more',
+		link: '/',
+	},
+	{
+		id: 2,
+		text: 'LOOKING FOR YOUR NEXT DREAM JOB?',
+		linkText: 'join us',
+		link: '/',
+	},
+	{
+		id: 3,
+		text: 'SEE HOW WE BUILT A PREMIUM DESIGN SYSTEM FOR NIKE.',
+		linkText: 'see more',
+		link: '/',
+	},
+]
