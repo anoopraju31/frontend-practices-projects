d1ee4fda4ad31391373b7cf25db5e78c4305c792
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 17:28:39 +0530

    Instrument Home Page: NavMarquee Component - animation added

--- instrument-home-page/src/components/NavMarquee.jsx
+++ instrument-home-page/src/components/NavMarquee.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { HiOutlineArrowSmRight } from 'react-icons/hi'
+import { motion, AnimatePresence } from 'framer-motion'
 import { marqueeText } from '../utills/navMarqueeText'
 
 const MarqueeLink = ({textId}) => (
-    <a href={marqueeText[textId].link} className='flex items-center text-gray-400'>
+    <a href={marqueeText[textId].link} className='flex items-center text-gray-400 uppercase'>
         <span className='underline'> {marqueeText[textId].linkText} </span>
         <HiOutlineArrowSmRight size={20} />
     </a>
@@ -20,10 +21,21 @@ const NavMarquee = () => {
 		return () => clearInterval(interval)
 	}, [])
   return (
-    <div className='py-[3px] flex justify-center text-sm'>
-        <p className='mr-2'> {marqueeText[id].text} </p>
-        <MarqueeLink textId={id} />
-    </div>
+    <AnimatePresence>
+      <motion.div 
+        initial={{ y: 5, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -5, opacity: 0 }}
+        transition={{
+          duration: 1,
+          repeatDelay: 9,
+          repeat: Infinity,
+        }}
+        className='py-[3px] flex justify-center text-sm'>
+          <p className='mr-2'> {marqueeText[id].text} </p>
+          <MarqueeLink textId={id} />
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
