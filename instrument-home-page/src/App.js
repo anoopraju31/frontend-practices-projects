547579fde65226276690bad5a536763ca32e7290
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 10:12:02 +0530

    Instrument Home Page: Home Page - initial

--- instrument-home-page/src/App.js
+++ instrument-home-page/src/App.js
@@ -1,7 +1,9 @@
+import { Home } from './Pages'
+
 const App = () => {
 	return (
-		<div className='App'>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+		<div className=''>
+			<Home />
 		</div>
 	)
 }
