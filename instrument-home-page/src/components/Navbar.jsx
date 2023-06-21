940f1408bfaa3783aafaaf41c2bdfa806a96bc37
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 12:25:29 +0530

    Instrument Home Page: NavLink Component - initial

--- instrument-home-page/src/components/Navbar.jsx
+++ instrument-home-page/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {NavLink} from '.'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className=' bg-orange-300'>
+            <nav className='px-hori py-verti flex'>
+                <div className='flex items-center gap-3'>
+                    <NavLink />
+                    <NavLink />
+                    <NavLink />
+                </div>
+
+
+                <div className='flex items-center gap-3'>
+                    <NavLink />
+                    <NavLink />
+                    <NavLink />
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
--- instrument-home-page/tailwind.config.js
+++ instrument-home-page/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			padding: {
+				hori: 'var(--padding-x)',
+				verti: 'var(--padding-y)',
+			},
+			margin: {
+				hori: 'var(--padding-x)',
+				verti: 'var(--padding-y)',
+			},
+		},
 	},
 	plugins: [],
 }
