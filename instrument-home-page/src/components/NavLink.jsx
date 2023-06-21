1f80becf4f33d60acb1d02a7d597c9286f6d0654
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 17:34:33 +0530

    Instrument Home Page: NavLink Component - animation added

--- instrument-home-page/src/components/NavLink.jsx
+++ instrument-home-page/src/components/NavLink.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const NavLink = ({title, link}) => {
   return (
-    <li>
+    <motion.li 
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 1, delay: 1 }}
+    >
         <a 
             className='px-4 py-[3px] flex items-center rounded-3xl bg-gray-200 text-xs hover:bg-black hover:text-white hover:rounded-lg transition-all'
             href={link}
         > 
             <span className='leading-relaxed uppercase'> {title}  </span>
         </a>
-    </li>
+    </motion.li>
   )
 }
 
