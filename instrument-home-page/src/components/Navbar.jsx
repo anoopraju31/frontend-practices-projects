6ae947c714c320e584b36ffc4ad9c5914402f18e
anoopraju31 <anoopraju31@gmail.com>
2023-06-21 14:01:28 +0530

    Instrument Home Page: Navbar Component - responsive styling 1

--- instrument-home-page/src/components/Navbar.jsx
+++ instrument-home-page/src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
+import { VscMenu } from 'react-icons/vsc'
 import {NavLink, NavMarquee} from '.'
 
 const Navbar = () => {
     return (
         <div className=''>
             <nav className='px-hori py-verti flex'>
-                <ul className='flex items-center gap-3'>
+                <ul className='flex items-center gap-6'>
                     <NavLink link='/work' title='Work'/>
                     <NavLink link='/service' title='Service'/>
                     <NavLink link='/about' title='About'/>
+                    <div className='block lg:hidden'> <VscMenu size={20} /> </div>
                 </ul>
 
-                <div className='flex-1'>
+                <div className='hidden lg:block flex-1'>
                     <NavMarquee />
                 </div>
 
-                <ul className='flex items-center gap-3'>
+                <ul className='hidden lg:flex items-center gap-3'>
                     <NavLink link='/careers' title='careers'/>
                     <NavLink link='/latest' title='latest'/>
                     <NavLink link='/contact' title='contact'/>
